9b2a82abf78a3688484f19923c5e86d8a6030fd4
Rajdeep Kanzariya <rajdeepkanzariya054@gmail.com>
2026-04-25 13:35:01 +0530

    auto push

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 import express from 'express';
 import cors from 'cors';
-import adminRoutes from './routes/routesData.js'; // Extension .js zaroori hai
+import path from 'path';
+import { fileURLToPath } from 'url';
+import adminRoutes from './routes/routesData.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// API Routes
 app.use('/api/admins', adminRoutes);
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+// Static files (React Build)
+const distPath = path.resolve(__dirname, 'dist');
+app.use(express.static(distPath));
+
+// React routing
+app.get('*', (req, res) => {
+    res.sendFile(path.join(distPath, 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Server + Frontend Running on port ${PORT}`);
 });
