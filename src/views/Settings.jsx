43b514921ba1990d5ab831df8c42929f6335c777
Rajdeep Kanzariya <rajdeepkanzariya054@gmail.com>
2026-05-20 15:57:46 +0530

    auto push

--- src/views/Settings.jsx
+++ src/views/Settings.jsx
@@ -408,41 +408,24 @@ const handleLogout = async () => {
     </span>
   </div>
 </div>
-{/* Export Tasks */}
+{/* Export Section */}
 <div style={{ ...S.sectionLabel, marginTop: 24 }}>EXPORT</div>
-
 <div style={S.card}>
-  <div
-    style={S.menuItem}
-    onClick={() => {
-      console.log("Export Tasks Clicked");
-    }}
-  >
+  <div style={S.menuItem} onClick={() => {}}>
     <div style={S.menuLeft}>
-      <div
-        style={{
-          ...S.menuIcon,
-          background: "rgba(15,137,137,0.15)",
-          color: "#0F8989"
-        }}
-      >
-        {/* Simple Download Icon */}
+      <div style={{ ...S.menuIcon, background: 'rgba(15,137,137,0.15)', color: '#0F8989' }}>
         <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" width="18" height="18">
           <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
           <polyline points="7 10 12 15 17 10"/>
           <line x1="12" y1="15" x2="12" y2="3"/>
         </svg>
       </div>
-
       <div>
         <div style={S.menuTitle}>Export Tasks</div>
-        <div style={S.menuSub}>Download tasks in CSV format</div>
+        <div style={S.menuSub}>Download as Excel or CSV</div>
       </div>
     </div>
-
-    <span style={S.chevron}>
-      <ChevronRight />
-    </span>
+    <span style={S.chevron}><ChevronRight /></span>
   </div>
 </div>
 {/* Account Section */}
